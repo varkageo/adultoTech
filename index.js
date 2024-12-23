203fc2490140030d31635a1f397e1a845004b2cf
Jesus Zerpa <56924492+varkageo@users.noreply.github.com>
2024-12-22 21:44:55 -0300

    Add files via upload
    this is the last commit, add new colors un contactanos section, new img and new js interaction

--- index.js
+++ index.js
@@ -26,3 +26,30 @@ function toggleCarrito() {
         carritoSection.style.visibility = "hidden";
     }
 }
+
+const buttonContact = document.getElementById('contactUs');
+
+buttonContact.addEventListener("click",()=>{
+    const message = document.getElementById('message');
+    setTimeout(()=>{
+        message.style.visibility = "visible";
+    },500)
+    setTimeout(()=>{
+        message.style.visibility = "hidden";
+    },6000)
+    console.log("hola funciona el click")
+    setTimeout(()=>{
+        location.reload();
+    },6001)
+})
+
+function compraste(){
+    const message = document.getElementById('message');
+    setTimeout(()=>{
+        message.style.visibility = "visible";
+    },500)
+
+    setTimeout(()=>{
+        message.style.visibility = "hidden";
+    },6000)
+}
